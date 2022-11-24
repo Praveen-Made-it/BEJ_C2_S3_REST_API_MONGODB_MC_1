909ad7c0eca5247fee10c8ed9755fe7b5ad16618
Praveen Kumar <praveen170596@gmail.com>
2022-11-24 19:09:02 +0530

    created attributes for Track class

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/domain/Track.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/domain/Track.java
@@ -7,5 +7,15 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Track {
+    @Id
+    private int trackId;
+    private String trackName;
+    private double trackRating;
+    private Artist trackArtist;
+
 }
