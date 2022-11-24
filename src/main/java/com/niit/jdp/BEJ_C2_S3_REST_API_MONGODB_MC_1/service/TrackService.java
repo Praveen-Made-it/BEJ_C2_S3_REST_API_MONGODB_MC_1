b5719ca31357f27e350254a8fd42b295149e884e
Praveen Kumar <praveen170596@gmail.com>
2022-11-24 19:29:34 +0530

    created TrackServiceImpl class

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/service/TrackService.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/service/TrackService.java
@@ -1,5 +1,17 @@
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.service;
 
-public interface TrackService {
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Track;
 
+import java.util.List;
+
+public interface TrackService {
+    Track saveTrack(Track track);
+
+    List<Track> getAllTracks();
+
+    boolean deleteTrack(int trackId);
+
+    List<Track> getAllTracksByRatingGreaterThan4();
+
+    List<Track> getAllTracksByArtistName(String artistName);
 }
