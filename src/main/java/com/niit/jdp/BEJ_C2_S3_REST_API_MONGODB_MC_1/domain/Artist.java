c82b5f8908491818e3a60a2230932e80ee1128fc
Praveen Kumar <praveen170596@gmail.com>
2022-11-24 19:05:13 +0530

    created getter and setter methods

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/domain/Artist.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/domain/Artist.java
@@ -18,4 +18,20 @@ public class Artist {
         this.artistId = artistId;
         this.artistName = artistName;
     }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
 }
