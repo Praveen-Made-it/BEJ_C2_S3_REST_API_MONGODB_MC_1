fc38428b595dee25c6d3ec0d358f9420249af3a9
Praveen Kumar <praveen170596@gmail.com>
2022-11-25 16:18:09 +0530

    Test case for saving the track success.

--- src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/service/TrackServiceImplTest.java
+++ src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/service/TrackServiceImplTest.java
@@ -7,5 +7,59 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.service;
 
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Artist;
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Track;
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.exception.TrackAlreadyExistsException;
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.repository.TrackRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+@ExtendWith(MockitoExtension.class)
 public class TrackServiceImplTest {
+    Artist artist1, artist2;
+    List<Track> trackList;
+    @Mock
+    private TrackRepository trackRepository;
+    @InjectMocks
+    private TrackServiceImpl trackService;
+    private Track track1, track2;
+
+    @BeforeEach
+    void setUp() {
+        artist1 = new Artist(1, "arjun");
+        artist2 = new Artist(2, "Sneha");
+        track1 = new Track(1, "chitra", 9, artist1);
+        track2 = new Track(2, "ragava", 5, artist2);
+        trackList = Arrays.asList(track1, track2);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    //Test case for saving the track success.
+    @Test
+    public void saveTrackSuccess() throws TrackAlreadyExistsException {
+        when(trackRepository.findById(track1.getTrackId())).thenReturn(Optional.ofNullable(null));
+        when(trackRepository.save(any())).thenReturn(track1);
+        assertEquals(track1, trackService.saveTrack(track1));
+        verify(trackRepository, times(1)).save(any());
+        verify(trackRepository, times(1)).findById(any());
+    }
+
 }
