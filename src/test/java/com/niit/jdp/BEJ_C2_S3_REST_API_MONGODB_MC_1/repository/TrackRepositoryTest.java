02ae1fc7bcfc908a9dac0c583c300ad0aa36f87f
Praveen Kumar <praveen170596@gmail.com>
2022-11-25 15:33:11 +0530

    Test case for retrieving all the track by Artist Name success

--- src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/repository/TrackRepositoryTest.java
+++ src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/repository/TrackRepositoryTest.java
@@ -7,5 +7,52 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.repository;
 
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Artist;
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Track;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataMongoTest
 public class TrackRepositoryTest {
+
+    @Autowired
+    private TrackRepository trackRepository;
+    private Artist artist;
+    private Track track;
+
+    @BeforeEach
+    void setUp() {
+        artist = new Artist(1, "Arun");
+        track = new Track(1, "donu", 7, artist);
+    }
+
+    @AfterEach
+    void tearDown() {
+        artist = null;
+        track = null;
+
+    }
+
+    @Test
+    // Test case for retrieving all the track by Artist Name success
+    public void getTrackByArtistNameSuccess() {
+        artist = new Artist(2, "Neha");
+        track = new Track(2, "Kala Chashma", 8, artist);
+        trackRepository.insert(track);
+        List<Track> tracks = trackRepository.findAllTracksFromArtistName(track.getTrackArtist().getArtistName());
+        assertEquals(1, tracks.size());
+        assertEquals(track.getTrackArtist().getArtistName(), tracks.get(0).getTrackArtist().getArtistName());
+    }
 }
+
+
