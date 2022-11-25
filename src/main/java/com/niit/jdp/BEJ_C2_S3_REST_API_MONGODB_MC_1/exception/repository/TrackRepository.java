e6024fb8dce603178608a186eb58eaca1f8a4a57
Praveen Kumar <praveen170596@gmail.com>
2022-11-25 08:53:32 +0530

    final commit

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/exception/repository/TrackRepository.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/exception/repository/TrackRepository.java
@@ -1,11 +1,13 @@
-package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.repository;
+package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.exception.repository;
 
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Track;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TrackRepository extends MongoRepository<Track, Integer> {
 
     @Query("{'trackRating':{$gt:4}}")
