066d8230b0ce9ef82b3c2d7325fb13268b8c9e03
Praveen Kumar <praveen170596@gmail.com>
2022-11-28 14:55:26 +0530

    test case created for saveTrackSuccess

--- src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/controller/TrackControllerTest.java
+++ src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/controller/TrackControllerTest.java
@@ -7,17 +7,28 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Artist;
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Track;
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.service.TrackServiceImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class TrackControllerTest {
@@ -42,5 +53,29 @@ public class TrackControllerTest {
     @AfterEach
     void tearDown() {
         track = null;
+    }
+
+    private static String jsonToString(final Object ob) throws JsonProcessingException {
+        String result;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonContent = mapper.writeValueAsString(ob);
+            result = jsonContent;
+        } catch (JsonProcessingException e) {
+            result = "JSON processing error";
+        }
+
+        return result;
+    }
+
+    @Test
+    public void saveTrackSuccess() throws Exception {
+        when(trackService.saveTrack(any())).thenReturn(track);
+        mockMvc.perform(post("/trackData/track/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonToString(track)))
+                .andExpect(status().isCreated()).andDo(MockMvcResultHandlers.print());
+        verify(trackService, times(1)).saveTrack(any());
+
     }
 }
